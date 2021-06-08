f8d13a005af82a486589aeefe8184f1842ac3836
Yuborama <davidjohan2@hotmail.com>
2021-06-07 19:12:19 -0500

    cambios xxxxx

--- src/main/java/com/elecciones/controllers/CandidatoController.java
+++ src/main/java/com/elecciones/controllers/CandidatoController.java
@@ -16,6 +16,17 @@ public class CandidatoController {
     }
 
     @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+
+
+
+
+
+
     //metodo recuerda en la web solo hay gets, para los parametros un ejemplo /candidato/id se escribe /{id} para conseguir esta id haces en el parametro del metodo debajo del decorador @PathVariable Integer id por ejemplo
     // puedes revisar aqui para eso https://github.com/StacklyCode/stacklyCode-team-backend/blob/dev/src/main/java/com/example/stacklyteam/web/resources/SkillResources.java
     // para pedir un objeto @RequestBody y el tipo por ejemplo aqui en el create yo haria un @RequestBody Candidato candidato
