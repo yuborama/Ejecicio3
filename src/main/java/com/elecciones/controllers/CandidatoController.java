a87d353027a27639d13a462dfb5c3fba568ce61d
Yuborama <davidjohan2@hotmail.com>
2021-06-07 21:46:42 -0500

    deploy

--- src/main/java/com/elecciones/controllers/CandidatoController.java
+++ src/main/java/com/elecciones/controllers/CandidatoController.java
@@ -1,27 +1,56 @@
 package com.elecciones.controllers;
 
+import com.elecciones.entities.Candidato;
+import com.elecciones.entities.Eleccion;
+import com.elecciones.entities.Estamento;
+import com.elecciones.entities.Tipodocumento;
 import com.elecciones.services.CandidatoService;
+import com.elecciones.services.EleccionService;
+import com.elecciones.services.EstamentoService;
+import com.elecciones.services.TipodocumentoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+import java.util.List;
+
+@Controller
 @RequestMapping("/candidato")
 public class CandidatoController {
 
     private final CandidatoService candidatoService;
-
-    CandidatoController(CandidatoService candidatoService){
+    private final EstamentoService estamentoService;
+    private final TipodocumentoService tipodocumentoService;
+    private final EleccionService eleccionService;
+    private final CandidatoService candidatoService;
+    CandidatoController(CandidatoService candidatoService,EstamentoService estamentoService, TipodocumentoService tipodocumentoService,EleccionService eleccionService) {
         this.candidatoService = candidatoService;
+        this.estamentoService = estamentoService;
+        this.tipodocumentoService =tipodocumentoService;
+        this.eleccionService = eleccionService;
     }
 
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public String elecion(Model modelo){
+        List<Estamento> estamentos = estamentoService.findAll();
+        List<Tipodocumento> tdocumentos = tipodocumentoService.findAll();
+        List<Eleccion> procesos = eleccionService.findAll();
+
+        modelo.addAttribute("estamentos",estamentos);
+        modelo.addAttribute("tdocumentos",tdocumentos);
+        modelo.addAttribute("procesos",procesos);
+        return "_a";
     }
 
 
-
+    @GetMapping("/new")
+    public String muestra(Model modelo){
+        List<Candidato> candidatos=candidatoService.findAll();
+        modelo.addAttribute("candidatos",candidatos);
+        return "cambio";
+    }
 
 
 
